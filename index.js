5834725a36148c1c6fca0abd12c3635ab296db68
Rupankar Das <rupankardasofficial@gmail.com>
2023-09-19 14:17:04 +0530

    Update API and render file

--- index.js
+++ index.js
@@ -3,34 +3,20 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 const API_URL = "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency";
 const yourAPIKey = "0e78e61f2fmsh2b5744d9dbe86d5p17e05ajsn6a23cb375ca2";
 
 app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
-  try {
-    const response = await axios.get(API_URL, {
-      headers: {
-        "X-RapidAPI-Key": yourAPIKey,
-      },
-    });
-    const result = response.data;
-    console.log(result);
-    res.render("index.ejs", { data: result });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.post("/", async (req, res) => {
-  try {
     console.log(req.body);
     const have = req.body.country1;
     const want = req.body.country2;
@@ -43,12 +29,12 @@ app.post("/", async (req, res) => {
         have,
         want,
         amount,
-      },
-    });
-    const result = response.data;
-    console.log(result);
-    res.render("index.ejs", { value: result });
-  } catch (error) {
+      }});
+      const result = response.data;
+      console.log(result);
+  try {
+    res.render("index.ejs", { value: JSON.stringify(result.new_amount)});
+   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
       error: "No activities that match your criteria.",
